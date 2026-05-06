376d53d5e730997c0b3949c1ea95ea2dd1f2438f
ShubhamChandratre <chandratreshubham@gmail.com>
2026-05-06 22:30:57 +0530

    changes

--- RMI-1,2,3,4,5,6,7,8/ServerImpl.java
+++ RMI-1,2,3,4,5,6,7,8/ServerImpl.java
@@ -10,3 +10,14 @@ public class ServerImpl extends UnicastRemoteObject implements ServerIntf
         return num1+num2;
     }
 }
+
+/*
+1. Addition: return num1+num2
+2. Multiplication: return num1*num2
+3. Division: return num1/num2
+4. Subtraction: return num1-num2
+5. Power of 2: return (int)Math.pov(2,num1)
+6. C to F: return (num1*9/5)+32
+7. Miles to Km: return (num1*1.60934)
+8. Hello Name: return "Hello"+name
+ */
